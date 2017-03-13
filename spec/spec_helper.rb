733083657839cf0627226c1ad0962def45ef815b
Joshua Tompkins <jtompkins@newrelic.com>
2017-03-13 09:14:39 -0700

    Pry should always be available. Allow test focusing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,11 @@ SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+end
+
 require 'reading_list'
+require 'pry'
 require_relative './resources/test_constants'
