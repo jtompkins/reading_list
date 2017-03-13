c5e8fcb98065328d0c92493ed3cce2fc68b2ffe2
Joshua Tompkins <jtompkins@newrelic.com>
2017-03-13 09:14:17 -0700

    Test object creation more completely.

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -6,5 +6,14 @@ describe ReadingList::Book do
       expect(ReadingList::Book.new(BOOK_HASH))
         .to be_a(ReadingList::Book)
     end
+
+    it 'fills in fields from the input hash' do
+      book = ReadingList::Book.new(BOOK_HASH)
+
+      expect(book.author).to eq(BOOK_HASH[:author])
+      expect(book.title).to eq(BOOK_HASH[:title])
+      expect(book.year).to eq(BOOK_HASH[:year])
+      expect(book.link).to eq(BOOK_HASH[:link])
+    end
   end
 end
--- spec/models/configuration_spec.rb
+++ spec/models/configuration_spec.rb
@@ -7,5 +7,13 @@ describe ReadingList::Configuration do
       expect(ReadingList::Configuration.new(CONFIG_HASH))
         .to be_a(ReadingList::Configuration)
     end
+
+    it 'fills in the fields correctly' do
+      config = ReadingList::Configuration.new(CONFIG_HASH)
+
+      expect(config.name).to eq(CONFIG_HASH['name'])
+      expect(config.email).to eq(CONFIG_HASH['email'])
+      expect(config.website).to eq(CONFIG_HASH['website'])
+    end
   end
 end
