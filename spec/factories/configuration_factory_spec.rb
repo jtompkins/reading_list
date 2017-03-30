989e5c7c9e843039d4666724e81b92bd10ab3a8c
Joshua Tompkins <jtompkins@newrelic.com>
2017-03-30 13:31:51 -0700

    Add tests that the config factory properly reads the YAML file.

--- spec/factories/configuration_factory_spec.rb
+++ spec/factories/configuration_factory_spec.rb
@@ -1,15 +1,24 @@
 require 'spec_helper'
 
 describe ReadingList::ConfigurationFactory do
-  let(:config_path) { 'spec/resources/test_config.yml' }
-
   subject(:factory) do
-    ReadingList::ConfigurationFactory.new(config_file: config_path)
+    ReadingList::ConfigurationFactory.new(config_file: CONFIG_FILE)
   end
 
   describe '#config' do
     it 'returns a Configuration instance' do
       expect(factory.config).to be_a(ReadingList::Configuration)
+    end
+
+    it 'fills in the values from the file' do
+      config = factory.config
+
+      expect(config.name).to eq('Test Name')
+      expect(config.email).to eq('test@example.com')
+      expect(config.website).to eq('https://www.example.com')
+      expect(config.book_file).to eq('spec/resources/test_books.json')
+      expect(config.theme).to eq('default')
+      expect(config.output_dir).to eq('~/reading_list_output')
     end
   end
 end
