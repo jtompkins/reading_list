4299057fd371c0e5b32cd845ed230744e2f77a07
Joshua Tompkins <jtompkins@newrelic.com>
2017-03-30 09:16:38 -0700

    Move themes to new location and fix broken tests.

--- lib/reading_list/factories/template_factory.rb
+++ lib/reading_list/factories/template_factory.rb
@@ -1,7 +1,7 @@
 module ReadingList
   class TemplateFactory
     def load(template)
-      IO.read(File.expand_path("../templates/#{template}", __dir__))
+      IO.read(File.expand_path("../resources/templates/#{template}", __dir__))
     end
   end
 end
